2b90df6282b82cfa473be12a4caeb0a96188f362
kaya <jepsonkaya@gmail.com>
2018-03-10 21:20:29 -0800

    updated allowed hosts

--- inventory_system/inventory_system/settings.py
+++ inventory_system/inventory_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6o19t-2fe$8iwiyrklwfg4d@1lv!k+c&emz96z6578-)9jq-#e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.4"]
 
 
 # Application definition
